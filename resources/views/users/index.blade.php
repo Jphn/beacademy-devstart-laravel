7891ec63e31a96447d651a8688d49cf25dfe0f19
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-03 09:15:19 -0300

    fix: identação das views

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,29 +1,29 @@
 @extends('template.users')
 @section('title', 'Listagem de Usuários')
 @section('body')
-	<section>
-		<h1>Listagem de Usuários</h1>
-		<table class="table">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>NOME</th>
-					<th>EMAIL</th>
-					<th>DATA DE CADASTRO</th>
-					<th>AÇÕES</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($users as $user)
-				<tr>
-					<th>{{ $user->id }}</th>
-					<td>{{ $user->name }}</td>
-					<td>{{ $user->email }}</td>
-					<td>{{ date('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
-					<td><a href="{{ route('users.show', $user->id) }}" class="btn btn-info text-white">Visualizar</a></td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</section>
+<section>
+	<h1>Listagem de Usuários</h1>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>NOME</th>
+				<th>EMAIL</th>
+				<th>DATA DE CADASTRO</th>
+				<th>AÇÕES</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($users as $user)
+			<tr>
+				<th>{{ $user->id }}</th>
+				<td>{{ $user->name }}</td>
+				<td>{{ $user->email }}</td>
+				<td>{{ date('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
+				<td><a href="{{ route('users.show', $user->id) }}" class="btn btn-info text-white">Visualizar</a></td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</section>
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,30 +1,30 @@
 @extends('template.users')
 @section('title', $user->name)
 @section('body')
-	<section>
-		<h1>Usuário - {{ $user->name }}</h1>
-		<table class="table">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>NOME</th>
-					<th>EMAIL</th>
-					<th>DATA DE CADASTRO</th>
-					<th>AÇÕES</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<th>{{ $user->id }}</th>
-					<td>{{ $user->name }}</td>
-					<td>{{ $user->email }}</td>
-					<td>{{ date('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
-					<td>
-						<a href="" class="btn btn-warning text-white">Editar</a>
-						<a href="" class="btn btn-danger text-white">Deletar</a>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</section>
+<section>
+	<h1>Usuário - {{ $user->name }}</h1>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>NOME</th>
+				<th>EMAIL</th>
+				<th>DATA DE CADASTRO</th>
+				<th>AÇÕES</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>{{ $user->id }}</th>
+				<td>{{ $user->name }}</td>
+				<td>{{ $user->email }}</td>
+				<td>{{ date('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
+				<td>
+					<a href="" class="btn btn-warning text-white">Editar</a>
+					<a href="" class="btn btn-danger text-white">Deletar</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</section>
 @endsection
