9357d64051b9bc7c5f2db80a7232fab1e9d075c5
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-03 08:58:21 -0300

    feat: template está funcionando

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,40 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Listagem de Usuários</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css
-" crossorigin="anonymous">
-</head>
-<body>
-	<main class="container">
-		<section>
-			<h1>Listagem de Usuários</h1>
-			<table class="table">
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>NOME</th>
-						<th>EMAIL</th>
-						<th>DATA DE CADASTRO</th>
-						<th>AÇÕES</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach ($users as $user)
-					<tr>
-						<th>{{ $user->id }}</th>
-						<td>{{ $user->name }}</td>
-						<td>{{ $user->email }}</td>
-						<td>{{ date('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
-						<td><a href="{{ route('users.show', $user->id) }}" class="btn btn-info text-white">Visualizar</a></td>
-					</tr>
-					@endforeach
-				</tbody>
-			</table>
-		</section>
-	</main>
-</body>
-</html>
+@extends('template.users')
+@section('title', 'Listagem de Usuários')
+@section('body')
+	<section>
+		<h1>Listagem de Usuários</h1>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>NOME</th>
+					<th>EMAIL</th>
+					<th>DATA DE CADASTRO</th>
+					<th>AÇÕES</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($users as $user)
+				<tr>
+					<th>{{ $user->id }}</th>
+					<td>{{ $user->name }}</td>
+					<td>{{ $user->email }}</td>
+					<td>{{ date('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
+					<td><a href="{{ route('users.show', $user->id) }}" class="btn btn-info text-white">Visualizar</a></td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</section>
+@endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,41 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>{{ $user->name }}</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css
-" crossorigin="anonymous">
-</head>
-<body>
-	<main class="container">
-		<section>
-			<h1>Usuário - {{ $user->name }}</h1>
-			<table class="table">
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>NOME</th>
-						<th>EMAIL</th>
-						<th>DATA DE CADASTRO</th>
-						<th>AÇÕES</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th>{{ $user->id }}</th>
-						<td>{{ $user->name }}</td>
-						<td>{{ $user->email }}</td>
-						<td>{{ date('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
-						<td>
-							<a href="" class="btn btn-warning text-white">Editar</a>
-							<a href="" class="btn btn-danger text-white">Deletar</a>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</section>
-	</main>
-</body>
-</html>
+@extends('template.users')
+@section('title', $user->name)
+@section('body')
+	<section>
+		<h1>Usuário - {{ $user->name }}</h1>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>NOME</th>
+					<th>EMAIL</th>
+					<th>DATA DE CADASTRO</th>
+					<th>AÇÕES</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>{{ $user->id }}</th>
+					<td>{{ $user->name }}</td>
+					<td>{{ $user->email }}</td>
+					<td>{{ date('d/m/Y - H:i', strtotime($user->created_at)) }}</td>
+					<td>
+						<a href="" class="btn btn-warning text-white">Editar</a>
+						<a href="" class="btn btn-danger text-white">Deletar</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+@endsection
