179ed413f75908668c33b067604479d932c52fe6
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-04 10:46:51 -0300

    feat: required adicionado aos campos

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -7,15 +7,15 @@
 	@csrf
 	<div class="mb-3">
 		<label for="name">Nome</label>
-		<input type="text" class="form-control" id="name" name="name" placeholder="Nome">
+		<input type="text" class="form-control" id="name" name="name" placeholder="Nome" required>
 	</div>
 	<div class="mb-3">
 		<label for="email">Email</label>
-		<input type="email" class="form-control" id="email" name="email" placeholder="Email@email.com">
+		<input type="email" class="form-control" id="email" name="email" placeholder="Email@email.com" required>
 	</div>
 	<div class="mb-3">
 		<label for="password">Senha</label>
-		<input type="password" class="form-control" id="password" name="password" placeholder="******">
+		<input type="password" class="form-control" id="password" name="password" placeholder="******" required>
 	</div>
 	<button type="submit" class="btn btn-primary">Enviar</button>
 </form>
