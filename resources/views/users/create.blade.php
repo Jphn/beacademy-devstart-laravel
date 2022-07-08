c702659a7b0bac7df6e5fd6d94cc8982982ea875
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-08 15:12:19 -0300

    `feat:` adição do campo de imagem nos formulários

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,24 +1,28 @@
 @extends('template.users')
 @section('title', 'Novo Usuário')
 @section('body')
-<h1>Novo Usuário</h1>
+	<h1>Novo Usuário</h1>
 
-@include('layout.error')
+	@include('layout.error')
 
-<form action="{{ route('user.store') }}" method="POST">
-	@csrf
-	<div class="mb-3">
-		<label for="name">Nome</label>
-		<input type="text" class="form-control" id="name" name="name" placeholder="Nome" required>
-	</div>
-	<div class="mb-3">
-		<label for="email">Email</label>
-		<input type="email" class="form-control" id="email" name="email" placeholder="Email@email.com" required>
-	</div>
-	<div class="mb-3">
-		<label for="password">Senha</label>
-		<input type="password" class="form-control" id="password" name="password" placeholder="******" required>
-	</div>
-	<button type="submit" class="btn btn-primary">Enviar</button>
-</form>
+	<form action="{{ route('user.store') }}" method="POST" enctype="multipart/form-data">
+		@csrf
+		<div class="mb-3">
+			<label for="name">Nome</label>
+			<input type="text" class="form-control" id="name" name="name" placeholder="Nome" required>
+		</div>
+		<div class="mb-3">
+			<label for="email">Email</label>
+			<input type="email" class="form-control" id="email" name="email" placeholder="Email@email.com" required>
+		</div>
+		<div class="mb-3">
+			<label for="password">Senha</label>
+			<input type="password" class="form-control" id="password" name="password" placeholder="********" required>
+		</div>
+		<div class="mb-3">
+			<label for="image">Selecione uma imagem</label>
+			<input type="file" class="form-control" id="image" name="image">
+		</div>
+		<button type="submit" class="btn btn-primary">Enviar</button>
+	</form>
 @endsection
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,25 +1,29 @@
 @extends('template.users')
 @section('title', "Editar - {$user->name}")
 @section('body')
-<h1>Novo Usuário</h1>
+	<h1>Novo Usuário</h1>
 
-@include('layout.error')
+	@include('layout.error')
 
-<form action="{{ route('user.update', $user->id) }}" method="POST">
-	@method('PUT')
-	@csrf
-	<div class="mb-3">
-		<label for="name">Nome</label>
-		<input type="text" class="form-control" id="name" name="name" value="{{$user->name}}" required>
-	</div>
-	<div class="mb-3">
-		<label for="email">Email</label>
-		<input type="email" class="form-control" id="email" name="email" value="{{$user->email}}" required>
-	</div>
-	<div class="mb-3">
-		<label for="password">Senha</label>
-		<input type="password" class="form-control" id="password" name="password">
-	</div>
-	<button type="submit" class="btn btn-primary">Enviar</button>
-</form>
+	<form action="{{ route('user.update', $user->id) }}" method="POST">
+		@method('PUT')
+		@csrf
+		<div class="mb-3">
+			<label for="name">Nome</label>
+			<input type="text" class="form-control" id="name" name="name" value="{{$user->name}}" required>
+		</div>
+		<div class="mb-3">
+			<label for="email">Email</label>
+			<input type="email" class="form-control" id="email" name="email" value="{{$user->email}}" required>
+		</div>
+		<div class="mb-3">
+			<label for="password">Senha</label>
+			<input type="password" class="form-control" id="password" name="password">
+		</div>
+		<div class="mb-3">
+			<label for="image">Selecione uma imagem</label>
+			<input type="file" class="form-control" id="image" name="image">
+		</div>
+		<button type="submit" class="btn btn-primary">Enviar</button>
+	</form>
 @endsection
